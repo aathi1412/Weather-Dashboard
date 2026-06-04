6374a8440ac1f2e6b088c37ed0264d015a9f97df
aathi1412 <itsmeaathi@gmail.com>
2026-06-04 12:36:24 +0530

    design 7 day forecast

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const setDate = document.querySelector('.js-date');
 setInterval(() => {
     let date = new Date();
     let time = date.getTime();
-    // let day = date.getDate();
-    // console.log(dayjs(day).format('DD-MM-YYYY'));
     setTime.innerHTML = dayjs(time).format('HH:mm A');
     setDate.innerHTML = dayjs(time).format('dddd, DD MMM');
 }, 1000)
@@ -16,4 +14,8 @@ const API_KEY = 'a856a832c1394edf622f5ec5b344a1dd';
 
 let api = 'https://api.openweathermap.org/data/4.0/onecall/current?lat={lat}&lon={lon}&appid={API key}';
 
-let geocoding = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+let geocoding = 'http://api.openweathermap.org/geo/1.0/direct?q=thiruneelakudi&limit=5&appid=a856a832c1394edf622f5ec5b344a1dd';
+
+
+
+// https://api.openweathermap.org/data/4.0/onecall/current?lat={lat}&lon={lon}&appid={a856a832c1394edf622f5ec5b344a1dd}
